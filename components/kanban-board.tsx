1435ea7367918fc2ca33d8f4f5c34eaadfb4d4e2
Souvik Maity <souvik.orbitup@gmail.com>
2026-05-19 11:21:09 +0530

    Implement job application editing, deleting and moving functionality with dialog support

--- components/kanban-board.tsx
+++ components/kanban-board.tsx
@@ -20,6 +20,7 @@ import {
 import { Button } from "./ui/button";
 import CreateJobApplicationDialog from "./create-job-dialog";
 import JobApplicationCard from "./job-application-card";
+import { useBoard } from "@/lib/hooks/useBoards";
 
 interface KanbanBoardProps {
   board: Board;
@@ -130,7 +131,7 @@ function SortableJobCard({
 }
 
 export default function KanbanBoard({ board, userId }: KanbanBoardProps) {
-  const columns = board.columns;
+  const { columns, moveJob } = useBoard(board);
 
   const sortedColumns = columns?.sort((a, b) => a.order - b.order) || [];
 
